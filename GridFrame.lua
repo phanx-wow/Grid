1ec2a63341a6c6cf16abf6bd9d2a5005b7a61270
Jerry <jerry@wowace>
2008-01-29 07:30:35 +0000

    put back the (corrected) fix for attributes. The bug still occurs. commit

--- GridFrame.lua
+++ GridFrame.lua
@@ -64,6 +64,8 @@ function GridFrame_OnAttributeChanged(self, name, value)
 			frame.unitName = nil
 			frame.unit = value
 		end
+	elseif name == "type1" and (not value or value == "") then
+		self:SetAttribute("type1", "target")
 	end
 end
 
