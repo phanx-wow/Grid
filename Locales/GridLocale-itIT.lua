2745e34703c06ebe1ae417766b409d68fc129b73
Phanx <addons@phanx.net>
2012-09-04 21:17:49 +0000

    - Add itIT localization file

---
+++ Locales/GridLocale-itIT.lua
@@ -0,0 +1,93 @@
+--[[--------------------------------------------------------------------
+	Grid
+	Compact party and raid unit frames.
+	Copyright (c) 2006-2012 Kyle Smith (a.k.a. Pastamancer), A. Kinley (a.k.a. Phanx) <addons@phanx.net>
+	All rights reserved.
+	See the accompanying README and LICENSE files for more information.
+	http://www.wowinterface.com/downloads/info5747-Grid.html
+	http://www.wowace.com/addons/grid/
+	http://www.curse.com/addons/wow/grid
+------------------------------------------------------------------------
+	GridLocale-itIT.lua
+	Italian (Italiano) localization.
+	Contributors: Holydeath1984, kappesante
+----------------------------------------------------------------------]]
+
+if GetLocale() ~= "itIT" then return end
+
+local _, Grid = ...
+local L = { }
+Grid.L = L
+
+------------------------------------------------------------------------
+--	GridCore
+
+--@localization(locale="itIT", namespace="GridCore", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridFrame
+
+--@localization(locale="itIT", namespace="GridFrame", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridLayout
+
+--@localization(locale="itIT", namespace="GridLayout", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridLayoutLayouts
+
+--@localization(locale="itIT", namespace="GridLayoutLayouts", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridLDB
+
+--@localization(locale="itIT", namespace="GridLDB", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridRoster
+
+--@localization(locale="itIT", namespace="GridRoster", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatus
+
+--@localization(locale="itIT", namespace="GridStatus", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusAggro
+
+--@localization(locale="itIT", namespace="GridStatusAggro", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusAuras
+
+--@localization(locale="itIT", namespace="GridStatusAuras", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusHeals
+
+--@localization(locale="itIT", namespace="GridStatusHeals", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusHealth
+
+--@localization(locale="itIT", namespace="GridStatusHealth", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusMana
+
+--@localization(locale="itIT", namespace="GridStatusMana", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusName
+
+--@localization(locale="itIT", namespace="GridStatusName", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusRange
+
+--@localization(locale="itIT", namespace="GridStatusRange", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusReadyCheck
+
+--@localization(locale="itIT", namespace="GridStatusReadyCheck", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusTarget
+
+--@localization(locale="itIT", namespace="GridStatusTarget", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusVehicle
+
+--@localization(locale="itIT", namespace="GridStatusVehicle", format="lua_additive_table")@
+------------------------------------------------------------------------
+--	GridStatusVoiceComm
+
+--@localization(locale="itIT", namespace="GridStatusVoiceComm", format="lua_additive_table")@
