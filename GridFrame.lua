fefcb8421df980c6a954aa9946cbbbe759af066b
Phanx <addons@phanx.net>
2013-03-07 03:13:14 +0000

    - Add temporary hack to silently fail instead of raising an error when an outdated plugin tries to access an old part of the options table.

--- GridFrame.lua
+++ GridFrame.lua
@@ -1305,6 +1305,13 @@ Grid.options.args.GridIndicator = {
 	args = {}
 }
 
+local fakeTableForOutdatedPlugins = {}
+setmetatable(Grid.options.args, { __index = function(t, k)
+	if k == "Indicator" then
+		return fakeTableForOutdatedPlugins
+	end
+end })
+
 ------------------------------------------------------------------------
 
 function GridFrame:PostInitialize()
